ac3a310d203695fd69d50cef1bb7e6962e086ab2
Nanoparty <nfootega@gmail.com>
2021-06-10 17:47:07 -0400

    first push

---
+++ project/messaging_api.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const WebSocket = require('ws');
+
+function createMessagingSocket() {
+    console.log("creating socket")
+    return new WebSocket('ws://localhost:3001/messages');
+}
+
+function getMessages() {
+    return axios.get('http://localhost:3001/messages').then(res => res.data);
+}
+
+function sendMessage(message) {
+    return axios.post('http://localhost:3001/messages', message);
+}
+
+module.exports.createMessagingSocket = createMessagingSocket;
+module.exports.getMessages = getMessages;
+module.exports.sendMessage = sendMessage;
